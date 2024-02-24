bb8852c7509130d3c5f32445b61648e7aa7cf6d1
nurrachmat <ahmfarisi@gmail.com>
2024-02-24 11:21:39 +0700

    Update main.dart

--- karyawan/lib/main.dart
+++ karyawan/lib/main.dart
@@ -12,18 +12,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            useMaterial3: true),
+        home: const MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+
+  Future<List<Karyawan>> _readJsonData() async {
+    final String response = await rootBundle.loadString('assets/karyawan.json');
+    final List<dynamic> data = json.decode(response);
+    return data.map((json) => Karyawan.fromJson(json)).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
