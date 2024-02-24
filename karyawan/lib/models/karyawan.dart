f772ac2d39f77f0169b80e32a54c6dee17a3fb8a
nurrachmat <ahmfarisi@gmail.com>
2024-02-24 10:59:43 +0700

    Update karyawan.dart

--- karyawan/lib/models/karyawan.dart
+++ karyawan/lib/models/karyawan.dart
@@ -1,4 +1,23 @@
-class Karyawan {}
+class Karyawan {
+  final String nama;
+  final int umur;
+  final Alamat alamat;
+  final List<String> hobi;
+
+  Karyawan(
+      {required this.nama,
+      required this.umur,
+      required this.alamat,
+      required this.hobi});
+
+  factory Karyawan.fromJson(Map<String, dynamic> json) {
+    return Karyawan(
+        nama: json['nama'],
+        umur: json['umur'],
+        alamat: Alamat.fromJson(json['alamat']),
+        hobi: List<String>.from(json['hobi']));
+  }
+}
 
 class Alamat {
   final String jalan;
