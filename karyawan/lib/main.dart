19e75ac02037a67d51ceea962ed4fe2358daff60
ahmfarisi <ahmfarisi@gmail.com>
2024-02-28 08:26:29 +0700

    Update main.dart

--- karyawan/lib/main.dart
+++ karyawan/lib/main.dart
@@ -44,18 +44,22 @@ class MyHomePage extends StatelessWidget {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                    snapshot.data![index].nama,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Umur: ${snapshot.data![index].umur}'),
-                      Text(
-                          'Alamat: ${snapshot.data![index].alamat.jalan}, ${snapshot.data![index].alamat.kota}, ${snapshot.data![index].alamat.provinsi}'),
-                    ],
+                return Card(
+                  child: ListTile(
+                    title: Text(
+                      snapshot.data![index].nama,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Umur: ${snapshot.data![index].umur}'),
+                        Text(
+                            'Alamat: ${snapshot.data![index].alamat.jalan}, ${snapshot.data![index].alamat.kota}, ${snapshot.data![index].alamat.provinsi}'),
+                        Text('Hobi: ${snapshot.data![index].hobi.join(', ')}'),
+                      ],
+                    ),
+                    leading: const FlutterLogo(size: 56.0),
                   ),
                 );
               },
