d375d9b5ed8ea54cffe380ee0a593e6283b1f5c9
Nur Rachmat <ahmfarisi@gmail.com>
2024-03-02 10:45:22 +0700

    Update home_screen.dart

--- pilem/lib/screens/home_screen.dart
+++ pilem/lib/screens/home_screen.dart
@@ -59,6 +59,52 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildMovieList(String title, List<Movie> movies) {
-    return Column();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final Movie movie = movies[index];
+              return GestureDetector(
+                //TODO: janganlupa onTap setelah detailscreen siap
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Image.network(
+                        'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                        height: 150,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        movie.title.length > 14
+                            ? '${movie.title.substring(0, 10)}...'
+                            : movie.title,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
   }
 }
