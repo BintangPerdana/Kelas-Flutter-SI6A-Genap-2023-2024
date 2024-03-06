6bcb0df33a3b7ce76f431c246d103bb2891dd668
ahmfarisi <ahmfarisi@gmail.com>
2024-03-06 09:17:44 +0700

    Update search_screen.dart

--- pilem/lib/screens/search_screen.dart
+++ pilem/lib/screens/search_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pilem/services/api_service.dart';
+import 'package:pilem/models/movie.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,6 +10,37 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final ApiService _apiService = ApiService();
+  final TextEditingController _searchController = TextEditingController();
+  List<Movie> _searchResult = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_searchMovies);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _searchMovies() async {
+    if (_searchController.text.isEmpty) {
+      setState(() {
+        _searchResult.clear();
+      });
+      return;
+    }
+
+    final List<Map<String, dynamic>> searchData =
+        await _apiService.searchMovies(_searchController.text);
+    setState(() {
+      _searchResult = searchData.map((e) => Movie.fromJson(e)).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
